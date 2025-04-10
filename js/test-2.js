028e0b06078b274d326ae5f92226d8f512e0b68f
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-04-10 22:20:40 +0500

    up

--- js/test-2.js
+++ js/test-2.js
@@ -1,10 +1,4 @@
-// 13-vazifa
-// let arr=[4, 7, 10, 15, 20];
-// const len1=arr.length
-// sum=0
-// for (let i=0;i<len1;i++){
-//     if(arr[i]%2==0){
-//         console.log("+"+arr[i])
-//         sum+=arr[i]
-//     }
-// }console.log("="+sum)
+// 14-vazifa
+const arr = [1, 2, 3, 4, 5];
+let arr2 = arr.reverse()
+console.log(arr2)
