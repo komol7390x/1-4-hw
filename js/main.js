eee0b2ebe7295f1b35852541b5d2b451c2fe97cd
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-04-10 20:29:06 +0500

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,67 @@
+// 1-vazifa
+// let son1=parseInt(prompt("Birinchi soni kiriting?"))
+// let son2=parseInt(prompt("Ikkinchi soni kiriting?"))
+// if(son1>son2){
+//     console.log(`${son1} katta ${son2} dan`)
+// }else if(son1<son2){
+//     console.log(`${son2} katta ${son1} dan`)
+// }else if(son1==son2){
+//     console.log("Ikkisi teng.")
+// }
+
+// 2-vazifa
+// let son=parseInt(prompt("Soni kiriting?"))
+// if(son>0){
+//     console.log(`Musbat son: ${son}`)
+// }else if(son<0){
+//     console.log(`Manfiy son: ${son}`)
+// }else if(son==0){
+//     console.log(`Nol son: ${son}`)
+// }
+
+// 3-vazifa
+// let son1=parseInt(prompt("Birinchi soni kiriting?"))
+// let son2=parseInt(prompt("Ikkinchi soni kiriting?"))
+// let amal=prompt("Amal kiriting?(+,-,*,/)")
+// if(amal=="+"){
+//     console.log(`${son1}+${son2}=${son1+son2}`)
+// }else if(amal=="-"){
+//     console.log(`${son1}-${son2}=${son1-son2}`)
+// }else if(amal=="*"){
+//     console.log(`${son1}*${son2}=${son1*son2}`)
+// }else if(amal=="/" && son2!=0){
+//     console.log(`${son1}/${son2}=${son1/son2}`)
+// }else{
+//     console.log("Xato kiritish")
+// }
+
+// 4-vazifa
+// let son=parseInt(prompt("Soni kiriting?"))
+// let sum=1
+// for(let i=1;i<=son;i++){
+//     sum*=i
+// }console.log(`Natija: ${sum}`)
+
+// 5-vazifa
+// let n =parseInt(prompt("Son kiriting?"));
+// let a = 0;
+// let b = 1;
+// let temp
+// for (let i = 2; i <= n; i++) {
+//     temp = a + b;
+//     a = b;
+//     b = temp;
+//     }
+// console.log(temp)
+
+// 6-vazifa
+// let son=parseInt(prompt("Soni kiriting?"))  
+// if (son==0){
+//     console.log("0 son")
+// }else if(son%2==0){
+//     console.log("Juft son")
+// }else if(son%2!=0){
+//     console.log("Toq son")
+// }else{
+//     console.log("Xatolik bor")
+// }
