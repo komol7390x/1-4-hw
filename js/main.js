9d2626d061e707f583ba8789cf374044dc5b10b5
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-04-10 20:56:02 +0500

    up

--- js/main.js
+++ js/main.js
@@ -1,49 +1,49 @@
 // 1-vazifa
-// let son1=parseInt(prompt("Birinchi soni kiriting?"))
-// let son2=parseInt(prompt("Ikkinchi soni kiriting?"))
-// if(son1>son2){
-//     console.log(`${son1} katta ${son2} dan`)
-// }else if(son1<son2){
-//     console.log(`${son2} katta ${son1} dan`)
-// }else if(son1==son2){
+// let son11=parseInt(prompt("Birinchi son1i kiriting?"))
+// let son12=parseInt(prompt("Ikkinchi son1i kiriting?"))
+// if(son11>son12){
+//     console.log(`${son11} katta ${son12} dan`)
+// }else if(son11<son12){
+//     console.log(`${son12} katta ${son11} dan`)
+// }else if(son11==son12){
 //     console.log("Ikkisi teng.")
 // }
 
 // 2-vazifa
-// let son=parseInt(prompt("Soni kiriting?"))
-// if(son>0){
-//     console.log(`Musbat son: ${son}`)
-// }else if(son<0){
-//     console.log(`Manfiy son: ${son}`)
-// }else if(son==0){
-//     console.log(`Nol son: ${son}`)
+// let son1=parseInt(prompt("son1i kiriting?"))
+// if(son1>0){
+//     console.log(`Musbat son1: ${son1}`)
+// }else if(son1<0){
+//     console.log(`Manfiy son1: ${son1}`)
+// }else if(son1==0){
+//     console.log(`Nol son1: ${son1}`)
 // }
 
 // 3-vazifa
-// let son1=parseInt(prompt("Birinchi soni kiriting?"))
-// let son2=parseInt(prompt("Ikkinchi soni kiriting?"))
+// let son11=parseInt(prompt("Birinchi son1i kiriting?"))
+// let son12=parseInt(prompt("Ikkinchi son1i kiriting?"))
 // let amal=prompt("Amal kiriting?(+,-,*,/)")
 // if(amal=="+"){
-//     console.log(`${son1}+${son2}=${son1+son2}`)
+//     console.log(`${son11}+${son12}=${son11+son12}`)
 // }else if(amal=="-"){
-//     console.log(`${son1}-${son2}=${son1-son2}`)
+//     console.log(`${son11}-${son12}=${son11-son12}`)
 // }else if(amal=="*"){
-//     console.log(`${son1}*${son2}=${son1*son2}`)
-// }else if(amal=="/" && son2!=0){
-//     console.log(`${son1}/${son2}=${son1/son2}`)
+//     console.log(`${son11}*${son12}=${son11*son12}`)
+// }else if(amal=="/" && son12!=0){
+//     console.log(`${son11}/${son12}=${son11/son12}`)
 // }else{
 //     console.log("Xato kiritish")
 // }
 
 // 4-vazifa
-// let son=parseInt(prompt("Soni kiriting?"))
+// let son1=parseInt(prompt("son1i kiriting?"))
 // let sum=1
-// for(let i=1;i<=son;i++){
+// for(let i=1;i<=son1;i++){
 //     sum*=i
 // }console.log(`Natija: ${sum}`)
 
 // 5-vazifa
-// let n =parseInt(prompt("Son kiriting?"));
+// let n =parseInt(prompt("son1 kiriting?"));
 // let a = 0;
 // let b = 1;
 // let temp
@@ -55,19 +55,37 @@
 // console.log(temp)
 
 // 6-vazifa
-// let son=parseInt(prompt("Soni kiriting?"))  
-// if (son==0){
-//     console.log("0 son")
-// }else if(son%2==0){
-//     console.log("Juft son")
-// }else if(son%2!=0){
-//     console.log("Toq son")
+// let son1=parseInt(prompt("son1i kiriting?"))  
+// if (son1==0){
+//     console.log("0 son1")
+// }else if(son1%2==0){
+//     console.log("Juft son1")
+// }else if(son1%2!=0){
+//     console.log("Toq son1")
 // }else{
 //     console.log("Xatolik bor")
 // }
 
 // 7-vazifa
-let son=parseInt(prompt("Soni kiriting?")) 
-while(son>0){
-    
-}
+// let son1=parseInt(prompt("Birinchi kiriting?")) 
+// const son2=son1
+// let firstNumber=son1%10
+// son1/=10
+// son1=Math.floor(son1)
+
+//  let secondNumber=son1%10
+// son1/=10
+// son1=Math.floor(son1)
+
+// let thirdNumber=son1%10
+// son1/=10
+// son1=Math.floor(son1)
+
+// let sum=(firstNumber**3)+(secondNumber**3)+(thirdNumber**3)
+// if (sum==son2){
+//     console.log("Armstrong soni")
+// }else{
+//     console.log("Armstrong soni emas")
+// }
+
+// 8-vazifa
