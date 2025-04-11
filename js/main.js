8acd16e9944734556113d60bb09719275ef343e9
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-04-11 09:40:35 +0500

    up

--- js/main.js
+++ js/main.js
@@ -173,3 +173,15 @@
 //     }
 // }console.log("Yig'indisi: "+sum);
 
+// 17-vazifa
+// let sonlar = [8, 12, 10, 14];
+// let len1=sonlar.length
+// let sum=0
+// for(let i=0;i<len1;i++){
+//     sum+=sonlar[i]
+// }
+// const ort=sum/len1
+// if(ort>10){
+//     alert("O'rtacha qiymat 10 sonida oshib keti!")
+// }
+// console.log(`Umumiy summa: ${sum}\nO'rtacha qiymat: ${ort}`);
